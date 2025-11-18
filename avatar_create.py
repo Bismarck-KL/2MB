3a364f39f3303aa065bf582e76476e09d8851010
Bismarck Ng <bismarck4game@gmail.com>
2025-11-18 11:35:55 +0800

    feat(avatar_create-page): add back button functionality and improve docstring

--- avatar_create.py
+++ avatar_create.py
@@ -1,9 +1,14 @@
 import pygame
 
-from utils.color import TITLE
+from utils.color import TITLE, QUIT_BASE, QUIT_HOVER
+from utils.ui import Button
+
 
 class AvatarCreateScene:
-    """A simple placeholder game scene to demonstrate scene switching."""
+    """A simple placeholder game scene to demonstrate scene switching.
+
+    Adds a small 'Back' button to return to the main menu.
+    """
 
     def __init__(self, app):
         self.app = app
@@ -11,9 +16,18 @@ class AvatarCreateScene:
         self.font = app.font
         self.title_font = app.title_font
 
+        # back button (top-left)
+        self.back_rect = pygame.Rect(20, 20, 140, 48)
+        self.back_button = Button(self.back_rect, text="Back", font=self.font,
+                                  base_color=QUIT_BASE, hover_color=QUIT_HOVER)
+
     def handle_event(self, event):
+        # keyboard: Esc returns to menu
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            # return to menu
+            self.app.change_scene("MenuScene")
+
+        # back button click
+        if self.back_button.handle_event(event):
             self.app.change_scene("MenuScene")
 
     def update(self, dt):
@@ -25,4 +39,8 @@ class AvatarCreateScene:
         txt = self.title_font.render("Avatar Create Scene", True, TITLE)
         rect = txt.get_rect(center=(self.app.WIDTH // 2, self.app.HEIGHT // 2))
         self.screen.blit(txt, rect)
+
+        # draw back button
+        mouse_pos = pygame.mouse.get_pos()
+        self.back_button.draw(self.screen, mouse_pos)
 
