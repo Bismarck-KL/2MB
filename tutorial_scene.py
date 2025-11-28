fce3af62af033e5ccca838eaaef3307a9cc6d8a2
Qianrina <25102951g@connect.polyu.hk>
2025-11-28 23:53:54 +0800

    feat(tutorial_scene): update start and back button dimensions for improved layout

--- tutorial_scene.py
+++ tutorial_scene.py
@@ -17,8 +17,8 @@ class TutorialScene:
 
         # TO-DO(Qianrina): update the button layout, size and positions
         # button layout
-        start_btn_w, start_btn_h = 116, 127
-        back_btn_w, back_btn_h = 116, 127
+        start_btn_w, start_btn_h = 168, 78
+        back_btn_w, back_btn_h = 168, 78
         next_btn_w, next_btn_h = 116, 127
         prev_btn_w, prev_btn_h = 116, 127
 
